af167ef5978b88b0117ee90b32aabbdfe63614dd
Thibaut Peiffer <thibaut.peiffer@student.uclouvain.be>
2025-12-05 17:45:01 +0100

    Day 1 Part 2

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -2,13 +2,11 @@ with open("day1_part1_input.txt") as f:
     status = 50
     total = 0
     for line in f.readlines():
-        print(line)
         letter = line[0]
         if letter == 'L':
             status = (status - int(line[1:]) ) % 100
         else:
             status = (status + int(line[1:]) ) % 100
-        print(status)
         if status == 0:
             total += 1
 print(total)
---
+++ day1/day1_part2.py
@@ -0,0 +1,40 @@
+def countNrOfZeros(start, input):
+    nrOfZeros = 0
+
+    direction = input[0]
+    toTurn = int(input[1:])
+
+    #print("Angle before : " + str(start))
+
+    fullRotation = toTurn // 100
+    rest = toTurn % 100
+    nrOfZeros += fullRotation
+    #print("Number of full rotations : " + str(fullRotation))
+    #print("Still left to turn : " + str(rest))
+
+    if direction == 'R':
+        if rest >= 100 - start :
+            nrOfZeros += 1
+            #print("+1")
+        start = (start + rest) % 100
+
+    else :
+        if rest >= start and start != 0:
+            nrOfZeros += 1
+            #print("+1")
+        start = (start - rest) % 100
+
+    #print("Angle after : " + str(start))
+    return start, nrOfZeros
+
+with open("day1_part1_input.txt") as f:
+    lockAngle = 50
+    zerosCount = 0
+    for line in f.readlines():
+        #print("\nLine : " + line, end="")
+        a,b = countNrOfZeros(lockAngle, line)
+        lockAngle = a
+        zerosCount += b 
+
+print("Total zeros encountered : " + str(zerosCount))
+
